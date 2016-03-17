85baabf7200ef58861d25d99946e939adc086948
François <21504921@etu.unicaen.fr>
2016-03-17 15:53:09 +0100

    intro tp2

---
+++ tp2/function.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: François
+ * Date: 17/03/2016
+ * Time: 15:27
+ */
+
+function getHeader($startSession, $title)
+{
+    echo "<!DOCTYPE html>
+<html lang=\"fr\">
+<head>
+  <meta charset=\"UTF-8\">
+  <title>$title</title>
+  <link rel='stylesheet' type='text/css' href='style.css'>
+</head>
+<body>
+
+</body>
+</html>"
+}
