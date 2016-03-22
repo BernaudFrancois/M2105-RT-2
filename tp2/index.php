fd015cedd974019105fd9b3ab4178482d5ffbb48
François <21504921@etu.unicaen.fr>
2016-03-22 08:44:23 +0100

    ex1

--- tp2/index.php
+++ tp2/index.php
@@ -1,8 +1,22 @@
 <!DOCTYPE html>
 <?php
 include "function.php";
-getHeader(False,"test");
+getHeader(True,"test");
 ?>
+<div>
+    <form action="ex1Validation.php" method="post">
+        <label for="name">votre prénom :</label>
+        <input type="text" name="prenom" id="name" style="display: block;">
+        <a href="ex1Validation.php"  <input class="btn btn-default" type="submit" value="OK"></a>
+    </form>
+</div>
+<?php
+if (!empty($_POST["prenom"])){
+$user = $_POST["prenom"];
+echo "$user";
+}
+?>
+
 <a class="btn btn-default">test</a>
 <?php
 getFooter();
