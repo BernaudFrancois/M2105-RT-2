98a0d144b4ce123adb0c763ddae2dfb2f6f0cd98
François <21504921@etu.unicaen.fr>
2016-03-24 16:58:28 +0100

    ex4  début

---
+++ tp2/ex4/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<?php
+include "../function.php";
+getHeader(True,"test");
+
+?>
+<div>
+    <form action="cookie3.php" method="post">
+        <fieldset>
+            <legend>nav préféré</legend>
+            <input name="browser" value="Chrome" type="radio"><label>Chrome</label>
+            <input name="browser" value="firefox" type="radio"><label>firefox</label>
+            <input type="radio" name="browser" value="IE"><label>IE</label>
+            <input type="radio" name="browser" value="opera"><label>opera</label>
+            <input type="radio" name="browser" value="autre"><label>autre</label>
+            <input class="btn btn-default" type="submit" value="OK">
+        </fieldset>
+    </form>
+</div>
+<?php
+    ?>
+
+
+<?php
+getFooter();
+?>
