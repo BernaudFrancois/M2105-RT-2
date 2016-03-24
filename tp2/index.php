08c42edca6c69de2e78733a25f733a1050e9dc5d
François <21504921@etu.unicaen.fr>
2016-03-24 14:25:16 +0100

    24/03/16
    ex1

--- tp2/index.php
+++ tp2/index.php
@@ -4,18 +4,18 @@ include "function.php";
 getHeader(True,"test");
 ?>
 <div>
-    <form action="index.php" method="post">
+    <form action="ex1Validation.php" method="post">
         <label for="name">votre prénom :</label>
         <input type="text" name="prenom" id="name" style="display: block;">
         <input class="btn btn-default" type="submit" value="OK">
     </form>
 </div>
 <?php
-if (!empty($_POST["prenom"])) {
-    $_SESSION["user"] = $_POST["prenom"];
-    echo $_SESSION["user"];
-}
-?>
+//if (!empty($_POST["prenom"])) {
+//    $_SESSION["user"] = $_POST["prenom"];
+//    echo $_SESSION["user"];
+//}
+//?>
 
 
 <?php
