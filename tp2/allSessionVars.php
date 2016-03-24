c8a40346551b1015816b62dea5dba5332ef87246
François <21504921@etu.unicaen.fr>
2016-03-24 14:58:18 +0100

    24/03/16
    ex1

--- tp2/allSessionVars.php
+++ tp2/allSessionVars.php
@@ -2,9 +2,15 @@
 include "function.php";
 getHeader(True,"validation");
 
+echo "<h1> titre </h1>";
+
 if (!empty($_POST["prenom"])) {
     $_SESSION["user"] = $_POST["prenom"];
-    echo $_SESSION["user"];
+//    echo $_SESSION["user"];
+}
+
+foreach ($_SESSION as $value){
+    echo "<p>valeur de la session : {$value} </p>";
 }
 
 
--- tp2/index.php
+++ tp2/index.php
@@ -2,20 +2,18 @@
 <?php
 include "function.php";
 getHeader(True,"test");
+
 ?>
 <div>
-    <form action="ex1Validation.php" method="post">
+    <form action="allSessionVars.php" method="post">
         <label for="name">votre prénom :</label>
         <input type="text" name="prenom" id="name" style="display: block;">
         <input class="btn btn-default" type="submit" value="OK">
     </form>
 </div>
 <?php
-//if (!empty($_POST["prenom"])) {
-//    $_SESSION["user"] = $_POST["prenom"];
-//    echo $_SESSION["user"];
-//}
-//?>
+
+    ?>
 
 
 <?php
