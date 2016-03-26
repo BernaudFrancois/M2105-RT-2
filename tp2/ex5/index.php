c8df0c5fb2760f4f52e4d3be9029378ff1fe2608
François <21504921@etu.unicaen.fr>
2016-03-26 22:21:43 +0100

    ex5

---
+++ tp2/ex5/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php
+include "../function.php";
+getHeader(True,"test");
+session_destroy();
+?>
+<div class="container">
+    <div class="alert alert-info">
+        <p>vous avez été correctement déco</p>
+        <a class="btn btn-default" href="../ex2/index.php">se connecter a nouveau</a>
+    </div>
+
+</div>
+<?php
+?>
+
+
+<?php
+getFooter();
+?>
